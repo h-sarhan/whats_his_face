9880a5e8be7b7491aeedf6a5326e0b5d7fecb4c8
aishwarya201 <aishwaryanov1999@gmail.com>
2019-12-07 18:23:54 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from facenet_pytorch import InceptionResnetV1
+import tensorflow as tf
+from mtcnn import MTCNN
+import numpy
+import torch
+import cv2
+import os
+
+detector = MTCNN()
+model = tf.keras.models.load_model('model.h5')
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+img = cv2.imread('train_imgs/siddhant_nair/IMG_0846.jpg')
+names = []
+for folder in os.listdir('train_imgs'):
+    filepath = 'train_imgs/' + folder
+    if os.path.isdir(filepath):
+        names.append(folder)
+
+def video_test():
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, frame = cap.read()
+        image = cv2.resize(frame, (640, 360))
+        faces = detector.detect_faces(image)
+        for result in faces:
+            x, y, w, h = result['box']
+            if (x > 0) & (y > 0) & (x + w < 640) & (y + h < 360):
+                face = image[y:y + h, x:x + w]
+                face = cv2.resize(face, (160, 160))
+                face = face.transpose(2, 0, 1).astype(numpy.float32)
+
+                mean, std = face.mean(), face.std()
+                face = (face - mean) / std
+                face = torch.from_numpy(numpy.expand_dims(face, axis=0))
+
+                embedding = resnet(face).cpu().detach().numpy()
+                prediction = names[numpy.argmax(model.predict([[embedding]]))]
+                a = model.predict([[embedding]])
+                num = max(a[0][0].tolist())
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                if num > 0.98:
+                    cv2.putText(image, f'{prediction} {round(num, 2)}%', (x, y - 10), font,
+                                0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                else:
+                    cv2.putText(image, f'Unknown {round(num, 2)}%', (x, y - 10), font,
+                                0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        cv2.imshow('frame', image)
+        if cv2.waitKey(16) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    video_test()
---
+++ train.py
@@ -0,0 +1,60 @@
+from facenet_pytorch import InceptionResnetV1
+import tensorflow as tf
+import numpy as np
+from mtcnn import MTCNN
+import torch
+import cv2
+import os
+
+names = []
+embeddings = []
+names_labels = []
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+models = tf.keras.models
+layers = tf.keras.layers
+detector = MTCNN()
+resnet = InceptionResnetV1(pretrained='vggface2').eval()
+
+print('loading images from database')
+for folder in os.listdir('train_imgs'):
+    filepath = 'train_imgs/' + folder
+    if os.path.isdir(filepath):
+        names_labels.append(folder)
+        for file_item in os.listdir(filepath):
+            if not os.path.isdir(filepath + '/' + file_item):
+                image = cv2.imread(filepath + '/' + file_item)
+                if not image is None:
+                    x, y, w, h = detector.detect_faces(image)[0]['box']
+                    face = image[y:y + h, x:x + w]
+                    face = cv2.resize(face, (160, 160))
+                    face = face.transpose(2, 0, 1).astype(np.float32)
+
+                    mean, std = face.mean(), face.std()
+                    face = (face - mean) / std
+                    face = torch.from_numpy(np.expand_dims(face, axis=0))
+
+                    embedding = resnet(face).cpu().detach().numpy()
+                    embeddings.append(embedding)
+                    names.append([folder])
+
+print('training image face classifier')
+print(names_labels)
+embeddings = np.asarray(embeddings)
+names = np.asarray(names)
+for i in range(len(names)):
+    for j in range(len(names[i])):
+        names[i][j] = names_labels.index(names[i][j])
+
+names = names.astype(np.int64)
+model = models.Sequential()
+model.add(layers.Dense(512, input_shape=(1, 512), activation='relu'))
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(len(names_labels), activation='softmax'))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+history = model.fit(embeddings, names, epochs=50)
+model.save('model.h5')
